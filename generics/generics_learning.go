f070cd4fc978dcd128f0debd2b0c2fa43419dfab
v_liuxingkai <v_liuxingkai@bilibili.com>
2023-02-08 11:49:16 +0800

    feat(golang-tutorial) : 《Getting start with generics》 section：Declare a type constraint ； 新增一些行注释； 其中多运行几次可以发现go中的浮点数特征 float64（.999999）

--- generics/generics_learning.go
+++ generics/generics_learning.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+type Number interface{
+	int64 | float64
+}
+
 func main() {
 	// Initialize a map for the integer values
 	ints := map[string]int64{
@@ -19,13 +23,25 @@ func main() {
 		"3rd" : 47.03,
 	}
 
+	// 不使用泛型 为每一种类型添加一种同一过程的方法
 	fmt.Printf("Non-Generic Sums: %v and %v \n",
 		SumInts(ints),
 		SumFloats(floats))
 
+	// 使用泛型 并在调用方法的时候 声明符合方法中定义的泛型里具体的一种
 	fmt.Printf("Generic Sums: %v and %v \n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	// 使用泛型 并在调用方法的时候不进行声明泛型的具体类型 由编译器自行推断
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v \n",
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
+
+	// 使用定义的Number的泛型
+	fmt.Printf("Genric Sums with Constraint: %v and %v \n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 // SumInts adds together the values of m.
 // 整数累加 map的集合
@@ -50,9 +66,20 @@ func SumFloats(m map[string]float64) float64 {
 
 // SumIntsOfFloats sums the values of map m.
 // It supports both int64 and float64 as types for map values.
+// 泛型的数据累加 map的集合
 // comparable -- map的key值必须为comparable的；任何可以用运算符“==”和“！=”操作的类型
 // 由于已经定义了K和V的泛型 故 m变量是可以使用的
 func SumIntsOrFloats[K comparable, V int64 | float64] (m map[K]V) V {
+	var s V
+	for _, v := range m{
+		s += v
+	}
+	return s
+}
+
+// SumNumbers sums the values of map m, It supports both integers and floats as map values.
+// 支持类型Number（上文定义的）泛型的数据累加 map的集合
+func SumNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m{
 		s += v
