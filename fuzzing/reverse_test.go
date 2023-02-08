078bd865547aee3da016db18432dd392271755bc
v_liuxingkai <v_liuxingkai@bilibili.com>
2023-02-08 17:26:24 +0800

    feat(golang-tutorial):《Getting start with fuzzing》 section: fix the invalid string error ;由于使用的是fuzz的测试 并将测试用例输出至了新文件中 该bug展示了个别Unicode字符不单单占用一个字节的事实；以及在命令行中使用log来记录临时输出。

--- fuzzing/reverse_test.go
+++ fuzzing/reverse_test.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"testing"
+	"unicode/utf8"
 )
 
 func TestReverse(t *testing.T) {
@@ -19,4 +20,30 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string {"Hello, world!", "  ", "1234567!"}
+
+	for _, tc := range testcases {
+		// use f.Add to provide a seed corpus
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		// log
+		t.Logf("Number of runes: orig:%d, rev:%d, doubleRev:%d.", 
+			utf8.RuneCountInString(orig),
+			utf8.RuneCountInString(rev),
+			utf8.RuneCountInString(doubleRev))
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
