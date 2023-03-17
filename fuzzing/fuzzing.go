41b7ad95f47b84fb510b4b1ad7878b9c405cf950
v_liuxingkai <v_liuxingkai@bilibili.com>
2023-03-17 11:00:23 +0800

    feat(reflect的使用): 使用reflect定义指定map
    自定义结构体 通过reflect将格式化数据解析并封装到map结构中 以方便使用

--- fuzzing/fuzzing.go
+++ fuzzing/fuzzing.go
@@ -1,27 +1,26 @@
 package main
 
-import(
+import (
+	"errors"
 	"fmt"
 	"unicode/utf8"
-	"errors"
 )
 
 func Reverse(s string) (string, error) {
 	// 对输入的字符串进行Unicode的检查
-	if !utf8.ValidString(s){
+	if !utf8.ValidString(s) {
 		return s, errors.New("the input value is not Unicode for not valide UTF-8")
 	}
 	// fmt.Printf("the input: %q \n", s)
 	// b := []byte(s) // bug fixed 事实上有用不止一个byte来表示字符的unicode
-	b := []rune(s)  // bug go在将string转化成rune时 可能会将单字节但是非有效的unicode的string转化成unicode中代表未知字符的字符集 该操作就改变了双反转与原字符串相等的结果
+	b := []rune(s) // bug go在将string转化成rune时 可能会将单字节但是非有效的unicode的string转化成unicode中代表未知字符的字符集 该操作就改变了双反转与原字符串相等的结果
 	// fmt.Printf("the rune: %q \n", b)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i + 1, j - 1{
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
-	} 
+	}
 
 	return string(b), nil
 }
-
 
 func main() {
 	input := "the quick brown fox jumped over the lazy dog"
---
+++ fuzzing/slice_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+type sliceTest struct {
+	Id       int
+	IntSlice []int
+	Name     string
+}
+
+func TestAppendIntSlice(t *testing.T) {
+	//mgr := GetExcelUtilMgr()
+	//mgr.readExcelFile(excel.EXCEL_RHYTHM_MASTER_CONFIG)
+	intSlice1 := make([]int, 5)
+	var intSlice2 []int
+	data := []int{1, 2, 3, 4, 5}
+
+	for _, datum := range data {
+		intSlice1 = append(intSlice1, datum)
+		intSlice2 = append(intSlice2, datum)
+	}
+
+	fmt.Printf("the intSlict1 is : %v \n", intSlice1)
+	fmt.Printf("the intSlict2 is : %v \n", intSlice2)
+}
+
+func TestReflectAppendSlice(t *testing.T) {
+	dataIntSlice := []string{"1;3;5;7;9;11", "2;4;6;8;10"}
+	ids := []int{1, 2}
+	typeTCD := reflect.TypeOf(sliceTest{})                        // 获取struct的类型
+	targetMapType := reflect.MapOf(reflect.TypeOf(1), typeTCD)    // 定义map构成类型  key是什么类型 value是什么类型
+	targetMap := reflect.MakeMapWithSize(targetMapType, len(ids)) // 生成指定的map
+	for index, id := range ids {
+		singleTarget := reflect.New(typeTCD) // 指向该新对象的指针
+		for i := 0; i < typeTCD.NumField(); i++ {
+			fmt.Printf("the field.name is : %v \n", typeTCD.Field(i).Name)
+			switch typeTCD.Field(i).Name {
+			case "Id":
+				reflect.Indirect(singleTarget).FieldByName("Id").SetInt(int64(id)) // 指针取对象向对象内的字段进行赋值操作
+				break
+			case "Name":
+				reflect.Indirect(singleTarget).FieldByName("Name").SetString("testING")
+				break
+			case "IntSlice":
+				stringSlice := strings.Split(dataIntSlice[index], ";")
+				var intSlice []int // 仅声明slice 即空slice 若指定长度则会初始化各个元素为0
+				for _, v := range stringSlice {
+					vInt, e := strconv.Atoi(v)
+					if e != nil {
+						fmt.Printf("数组解析有误：%s\n", e)
+					}
+					intSlice = append(intSlice, vInt)
+				}
+				filedSlice := reflect.Indirect(singleTarget).FieldByName("IntSlice")   // 获取到对象内的原slice字段
+				newSlice := reflect.AppendSlice(filedSlice, reflect.ValueOf(intSlice)) // 将slice 拼接slice 生成新的slice
+				reflect.Indirect(singleTarget).FieldByName("IntSlice").Set(newSlice)
+				break
+			}
+		}
+		//targetMap.SetMapIndex(reflect.ValueOf(index), singleTarget)
+		targetMap.SetMapIndex(reflect.ValueOf(index), reflect.Indirect(singleTarget)) // 将对象放置到map中 注意不是将指针放入（结合上文定义的map类型）
+	}
+	fmt.Printf("the result is %+v \n", targetMap)
+
+}
