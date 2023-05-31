89c1c384d0e671ca830e772040e8fb0c68a07f51
v_liuxingkai <v_liuxingkai@bilibili.com>
2023-05-31 14:24:39 +0800

    feat(time): 计算两天之差
    time.sub 计算指定两天之差

--- time_tool/time_test.go
+++ time_tool/time_test.go
@@ -2,10 +2,19 @@ package time_tool
 
 import (
 	"testing"
+	"time"
 )
 
 func TestGetNextDayZeroTimeStampSecond(t *testing.T) {
 	toNextDateTimeZero := Get2NextDayZeroTimeSecond()
 	t.Logf("the time to next day zero time is : %f \n", toNextDateTimeZero.Seconds())
 	t.Logf("the time to next day zero time is : %q \n", toNextDateTimeZero)
+}
+
+// 计算指定的两天之差 单位：天
+func TestTimeSub2TargetDay(t *testing.T) {
+	today := time.Now()
+	targetDay := time.Date(today.Year(), time.February, 12, 0, 0, 0, 0, today.Location())
+	result := today.Sub(targetDay)
+	t.Logf("the result day is : %v", int(result.Hours()/24))
 }
